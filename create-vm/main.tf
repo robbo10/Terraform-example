8bfccc1e3ddfe291d2291bd7d66c9b820dfb3ceb
Kieran Robinson <krobn@allstate.com>
2018-08-03 15:50:09 +0100

    Adds security group and configures web server

--- create-vm/main.tf
+++ create-vm/main.tf
@@ -2,9 +2,26 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+resource "aws_security_group" "allow_instance_access" {
+  name = "allow_webpage_access"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+}
 resource "aws_instance" "example1" {
   ami           = "ami-d179ceac"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.allow_instance_access.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Happy Friday!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags {
     Name = "myVm"
